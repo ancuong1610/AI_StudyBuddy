63748eb823901d5d82b71dcd25835b79fb4e90a8
Quoc Cuong An <ancuong751@gmail.com>
2024-01-09 17:54:44 +0100

    TODO: done ADD: new libs for JS TODO: develope subject.hml and algo for the AI

--- StudyBuddy/app.py
+++ StudyBuddy/app.py
@@ -4,31 +4,32 @@ app = Flask(__name__)
 app.secret_key = 'hidden'  # Change this to a secure key in production
 
 # Sample data
-subjects = ["Introduction into AI"]
+subjects = ["Introduction into AI", "Math"]
+
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
     if request.method == 'POST':
         student_name = request.form['student_name']
         session['student_name'] = student_name
-        return render_template('home.html', subjects = subjects)
+        return redirect(url_for('home'))
     return render_template('welcome.html')
+
 
 @app.route('/home', methods=['GET'])
 def home():
     student_name = session.get('student_name', 'Guest')
-    saved_subjects = session.get('saved_subjects', [])
-    return render_template('home.html', student_name=student_name, saved_subjects=saved_subjects)
+    return render_template('home.html', student_name=student_name, subjects=subjects)
+
 
 @app.route('/add_subject', methods=['GET', 'POST'])
 def add_subject():
     if request.method == 'POST':
         subject_name = request.form['subject_name']
-        saved_subjects = session.get('saved_subjects', [])
-        saved_subjects.append(subject_name)
-        session['saved_subjects'] = saved_subjects
+        subjects.append(subject_name)
         return redirect(url_for('home'))
     return render_template('add_subject.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
